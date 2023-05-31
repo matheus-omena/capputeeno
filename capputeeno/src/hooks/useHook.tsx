ef1272cddb74f7a09b45d4c11c76d2fcf96561a8
Matheus Omena <omena.theus@live.com>
2023-05-31 14:21:09 -0300

    feat: initial products fetch

--- capputeeno/src/hooks/useHook.tsx
+++
@@ -1,6 +0,0 @@
-import { FilterContext } from "@/contexts/filter-context";
-import { useContext } from "react";
-
-export default function useFilter() {
-  return useContext(FilterContext);
-}
---
+++ capputeeno/src/types/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id: string;
+  name: string;
+  price_in_centes: number;
+  image_url: string;
+}
